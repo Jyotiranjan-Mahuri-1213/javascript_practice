e23f94832acf0bea4749f5903ce9d91dfe39cc22
Jyotiranjan-Mahuri-1213 <jyotiranjanmahuri@gmail.com>
2026-06-09 13:32:49 +0530

    day-1

---
+++ first.js
@@ -0,0 +1,37 @@
+// console.log("Hello js");
+// let val="cutm";
+// console.log(val);
+// let fees=95000;
+// console.log(fees);
+
+    // Primitive data type
+let name ="jyoti";//string
+console.log("name : "+name);
+
+let age = 22; //number
+console.log("age : " +age);
+
+let course; // undefined
+console.log("course: ");
+
+let pass=Boolean(true);//boolean
+console.log("passed: "+pass);
+
+let marks =null;// null - absence of object
+console.log("marks : "+ marks);
+
+let x=BigInt(1245);//Bigint
+console.log(x);
+
+let y=Symbol('hello');//Symbol
+console.log(y);
+
+// creating object (Non-primitive data type)
+const student ={
+    name : "Jyotiranjan",
+    age : 22,
+    branch: "mca",
+    cgpa: 8.5,
+    isPass: true,
+};
+console.log(student);
